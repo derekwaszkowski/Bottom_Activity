89d398e43f8a25aba6c0033b99ab0cafce976678
Derek Waszkowski <derekwaszkowski@gmail.com>
2019-09-10 22:34:41 -0400

    Initial commit

---
+++ app/src/main/java/com/waszkowski/bottomactivity/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.waszkowski.bottomactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    private Button b1;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        b1 = v.findViewById(R.id.buttonToMap);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
+    }
+
+
+}
